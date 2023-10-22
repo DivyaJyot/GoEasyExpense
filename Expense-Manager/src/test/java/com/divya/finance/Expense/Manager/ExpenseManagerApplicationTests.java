07a4712c04557cbba7cf2dbd5d004cd45e9a781b
Divya Jyoti <40665584+DivyaJyot@users.noreply.github.com>
2023-10-22 18:11:22 +0530

    Initial Structure added

---
+++ Expense-Manager/src/test/java/com/divya/finance/Expense/Manager/ExpenseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divya.finance.Expense.Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
